dac2f0926652d1bb9fd2d9a51ddb5ec396a53670
Strelok <1@1.com>
2025-12-02 12:39:02 +0200

    Add blocklist script

---
+++ scripts/blocklist-update.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from datetime import datetime, timezone
+import re
+
+
+def load_domains(path: Path) -> set:
+    domains = set()
+    for txt_file in path.rglob("*.txt"):
+        with txt_file.open("r", encoding="utf-8") as f:
+            for line in f:
+                # Пропустити порожні та закоментовані рядки.
+                if not re.match(r"^[a-zA-Z0-9]", line):
+                    continue
+
+                # Вилучити домен з формату "0.0.0.0 domain.com"
+                if line.startswith("0.0.0.0 "):
+                    domain = line[8:].strip()
+                    if domain and " " not in domain:
+                        domains.add("0.0.0.0 " + domain.rstrip("\n"))
+                else:
+                    # Перевірка на валідність домену
+                    if re.match(r"^[a-zA-Z0-9][-a-zA-Z0-9.]*\.[a-zA-Z]{2,}$", line):
+                        domains.add("0.0.0.0 " + line.rstrip("\n"))
+    return domains
+
+
+def write_blocklist(domains, out_path):
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write("#------------------------[UPDATE]------------------------\n")
+        f.write("# Title: Blocklist\n")
+        f.write(
+            f'# Updated:: {datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")}\n'
+        )
+        f.write(f"# Total number of blocklist urls: {str(len(domains))}\n")
+        f.write("# -------------------------[URL]-------------------------\n")
+        for d in domains:
+            f.write(f"{d}\n")
+
+
+if __name__ == "__main__":
+    base_dir = Path(__file__).resolve().parent.parent
+    blocklist = load_domains(Path.joinpath(base_dir, "blocklist-src"))
+    # Path.joinpath(base_dir, "release").mkdir(exist_ok=True)
+    blocklist_path = str(Path.joinpath(base_dir, "blocklist.txt"))
+    write_blocklist(blocklist, blocklist_path)
